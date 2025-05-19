ea8539b9dff5717fbcbcc2c0fe270db27399698b
Heyan Zhu <zhu_heyan@163.com>
2025-05-19 22:30:01 +0800

    feat(window): add blur to the main window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,11 +10,21 @@ use std::sync::Mutex;
 mod types;
 use types::AppData;
 
+#[cfg(target_os = "macos")]
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
+#[cfg(target_os = "windows")]
+use tauri_plugin_blur::apply_blur;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     Builder::default()
 		.setup(|app| {
+			let window = app.get_webview_window("main").unwrap();
+			#[cfg(target_os = "macos")]
+			apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None).expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+			#[cfg(target_os = "windows")]
+			apply_blur(&window, Some((18, 18, 18, 125))).expect("Unsupported platform! 'apply_blur' is only supported on Windows");
 			app.manage(Mutex::new(AppData {
 				chat: Chat::new(app.handle())?,
 				diagram_cache: DiagramCache::new()?,
